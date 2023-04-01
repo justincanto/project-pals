4127a00fc2e63e4a4f3abd70e05f7d32da79f13c
Justin Canto <justinlcanto@gmail.com>
2023-04-01 14:39:20 +0200

    feat: user links in pal card w/ icons

--- src/components/palCard.tsx
+++ src/components/palCard.tsx
@@ -1,32 +1,67 @@
 import Image from "next/image";
 
 type PalCardProps = {
-  name: string | null;
-  description: string | null;
-  role: string | null;
-  image: string | null;
+  user: {
+    name: string | null;
+    description: string | null;
+    role: string | null;
+    image: string | null;
+    links: string[];
+  };
 };
 
-export const PalCard = ({ name, description, role, image }: PalCardProps) => {
+const USER_LINKS_ICONS = {
+  "linkedin.com": "linkedin",
+  "github.com": "github",
+  "twitter.com": "twitter",
+  "dribbble.com": "dribbble",
+  "behance.net": "behance",
+};
+
+const WEB_LINK_ICON = "web";
+
+const getIcon = (url: string) => {
+  const urlComponents = url.replace(/(https?:\/\/)?(www.)?/i, "").split("/");
+  const domainComponents = urlComponents[0]?.split(".") || [];
+  const icon =
+    domainComponents.slice(domainComponents.length - 2).join(".") || "";
+  return (
+    USER_LINKS_ICONS[icon as keyof typeof USER_LINKS_ICONS] ?? WEB_LINK_ICON
+  );
+};
+
+export const PalCard = ({ user }: PalCardProps) => {
   return (
     <div className="relative flex w-96 flex-col gap-2 rounded-lg bg-purple-600 bg-opacity-[.15] p-4 leading-snug text-gray-800">
       <div className="flex items-center justify-start gap-4">
-        {image ? (
+        {user.image ? (
           <Image
-            src={image}
-            alt={`${name || ""} profile picture`}
+            src={user.image}
+            alt={`${user.name || ""} profile picture`}
             width={80}
             height={80}
             className="rounded-full"
           />
         ) : null}
         <p>
-          <span className="text-lg font-bold">{name}</span>
+          <span className="text-lg font-bold">{user.name}</span>
           <br />
-          {role}
+          {user.role}
         </p>
       </div>
-      <p className="leading-tight">{description}</p>
+      <p className="leading-tight">{user.description}</p>
+      <div className="flex gap-x-2">
+        {user.links.map((link) => (
+          <a href={link} key={link} target="_blank" rel="noreferrer">
+            <Image
+              src={`/icons/${getIcon(link)}.svg`}
+              alt={getIcon(link) + " link"}
+              width={20}
+              height={20}
+            />
+          </a>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/palsList.tsx
+++ src/components/palsList.tsx
@@ -7,13 +7,7 @@ export const PalsList = () => {
   return (
     <div className="flex flex-wrap justify-center gap-6">
       {users.data?.map((user) => (
-        <PalCard
-          description={user.description}
-          name={user.name}
-          role={user.role}
-          image={user.image}
-          key={user.id}
-        />
+        <PalCard user={user} key={user.id} />
       ))}
     </div>
   );
