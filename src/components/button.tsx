08ea239729aa5536bfefbd965ba7fbad441fb650
Justin Canto <justinlcanto@gmail.com>
2023-04-08 19:19:16 +0200

    wip: responsive layout

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,6 +1,16 @@
-export const Button = ({ content }: { content: string }) => {
+export const Button = ({
+  content,
+  className,
+}: {
+  content: string;
+  className?: string;
+}) => {
   return (
-    <span className="block rounded-lg bg-purple-600 bg-opacity-50 px-6 py-2.5 text-gray-800 transition-colors hover:cursor-pointer hover:bg-opacity-70">
+    <span
+      className={`block rounded-lg bg-purple-600 bg-opacity-50 px-6 py-2.5 text-gray-800 transition-colors hover:cursor-pointer hover:bg-opacity-70 ${
+        className || ""
+      }`}
+    >
       {content}
     </span>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,20 +2,21 @@ import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { Button } from "./button";
 import { useRouter } from "next/router";
+import Image from "next/image";
 
 export const Header = () => {
   const session = useSession();
   const router = useRouter();
   return (
-    <div className="flex items-center justify-between px-8 py-4 text-gray-800">
-      <Link href="/" className="text-2xl font-bold">
+    <div className="flex items-baseline justify-between p-4 text-gray-800 sm:px-8 sm:py-4">
+      <Link href="/" className="text-xl font-bold sm:text-2xl">
         Project<span className="text-purple-600 text-opacity-70">Pals</span>
       </Link>
-      <div className="flex items-center gap-12 text-lg font-bold">
-        <nav className="flex items-center gap-x-12">
+      <div className="flex items-center gap-x-12 text-lg font-bold">
+        <nav className="flex items-center gap-x-4 sm:gap-x-6 md:gap-x-12">
           <Link
             href="/projects"
-            className={`hover:text-purple-600 hover:text-opacity-70 ${
+            className={`hidden hover:text-purple-600 hover:text-opacity-70 sm:block ${
               router.pathname == "/projects"
                 ? "text-purple-600 text-opacity-70"
                 : ""
@@ -25,7 +26,7 @@ export const Header = () => {
           </Link>
           <Link
             href="/pals"
-            className={`hover:text-purple-600 hover:text-opacity-70 ${
+            className={`hidden hover:text-purple-600 hover:text-opacity-70 sm:block ${
               router.pathname == "/pals"
                 ? "text-purple-600 text-opacity-70"
                 : ""
@@ -35,7 +36,7 @@ export const Header = () => {
           </Link>
           <Link
             href="/new-project"
-            className={`hover:text-purple-600 hover:text-opacity-70 ${
+            className={`hidden hover:text-purple-600 hover:text-opacity-70 sm:block ${
               router.pathname == "/new-project"
                 ? "text-purple-600 text-opacity-70"
                 : ""
@@ -56,9 +57,18 @@ export const Header = () => {
             </Link>
           ) : (
             <Link href="/account" className="font-normal">
-              <Button content="Login"></Button>
+              <Button content="Login" className="hidden sm:block"></Button>
+              <span className="flex text-base font-bold sm:hidden">Login</span>
             </Link>
           )}
+          <button className="block sm:hidden">
+            <Image
+              src="/icons/menu.svg"
+              width={24}
+              height={24}
+              alt="menu icon"
+            />
+          </button>
         </nav>
       </div>
     </div>
